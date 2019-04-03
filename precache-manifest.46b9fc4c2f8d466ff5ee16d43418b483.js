6d82f03e4cb103f166c1ff5ac3667a0b39cd0fea
bsrushti <bsrushti@in-bsrushti.local>
2019-04-03 17:44:32 +0530

    Updates

---
+++ precache-manifest.46b9fc4c2f8d466ff5ee16d43418b483.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adb69785f4df52a868f6",
+    "url": "/react-word-crossing/static/css/main.d3b8d1f1.chunk.css"
+  },
+  {
+    "revision": "adb69785f4df52a868f6",
+    "url": "/react-word-crossing/static/js/main.ce04232f.chunk.js"
+  },
+  {
+    "revision": "913160098daf065d99ca",
+    "url": "/react-word-crossing/static/js/runtime~main.31a5a2db.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-word-crossing/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "de10a751b9411d4e71ee8d6193317f1d",
+    "url": "/react-word-crossing/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-word-crossing/precache-manifest.46b9fc4c2f8d466ff5ee16d43418b483.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-word-crossing/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
